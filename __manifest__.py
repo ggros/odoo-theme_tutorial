83e32634bee68733992a7bba4917c41520abb3b1
docker-odoo <gui.gros@gmail.com>
2020-02-07 10:34:40 +0000

    empty theme with new header

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Gui Gros',
+
+  'data': [
+      'views/layout.xml',
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
